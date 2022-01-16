c8380f9c1e971a3a3925320043b4a1441151e9b8
Vincent Arlou <arlou@protonmail.com>
2022-01-16 01:50:50 -0500

    style: add theming func to styled-components 💄

--- client/src/styles/global.ts
+++ client/src/styles/global.ts
@@ -1,24 +1,87 @@
-/* istanbul ignore file */
 import { createGlobalStyle } from 'styled-components';
+import { reset } from 'styled-reset';
+import { bp, breakpoints } from 'styles/breakpoint';
+import theme, { rhythm } from 'styles/theme';
 
 const GlobalStyles = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+/* stylelint-disable no-duplicate-selectors */
+/* ---------------------------------------------
+Reset CSS
+--------------------------------------------- */
+${reset}
+html {
+  background-color: ${theme.color.lightest};
+  overflow-y: scroll;
+}
+html {
+  text-size-adjust: 100%;
+}
+audio, canvas, video, progress, picture {
+  display: inline-block;
+}
+template {
+  display: none;
+}
+textarea {
+  overflow: auto;
+  resize: vertical;
+  vertical-align: top;
+}
+::-moz-focus-inner {
+  border: 0;
+  padding: 0;
+}
+/* ---------------------------------------------
+ Base Element Settings
+--------------------------------------------- */
+* {
+  box-sizing: border-box;
+}
+html,
+body {
+  height: 100%;
+  width: 100%;
+}
+html {
+  font-size: 10px;
+}
+body {
+  font-size: 1.6rem;
+}
+button {
+  appearance: none;
+  background: transparent;
+  border: 0;
+  border-radius: 0;
+  cursor: pointer;
+  padding: 0;
+}
+a {
+  text-decoration: none;
+}
+/* stylelint-enable no-duplicate-selectors */
+/* ---------------------------------------------
+ css variables
+--------------------------------------------- */
+:root {
+  --container-padding: ${rhythm(3)};
+
+  ${bp(breakpoints.sm)} {
+    --container-padding: ${rhythm(5)};
   }
 
-  html {
-    font-size: 62.5%;
+  ${bp(breakpoints.md)} {
+    --container-padding: ${rhythm(8)};
   }
 
-  html, body, #__next {
-    height: 100%;
+  ${bp(breakpoints.lg)} {
+    --container-padding: ${rhythm(12)};
   }
 
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  ${bp(breakpoints.xl)} {
+    --container-padding: ${rhythm(15)};
   }
+}
 `;
 
 export default GlobalStyles;
---
+++ client/src/styles/theme/index.ts
@@ -0,0 +1,172 @@
+import { DefaultTheme } from 'styled-components';
+
+const RHYTHM = 8;
+
+export const toRems = (size: number): string => `${size / 10}rem`;
+export const rhythm = (mult: number): string => `${(mult * RHYTHM) / 10}rem`;
+export const aColor = (color: Colors, alpha: Alphas): string =>
+  `${colors[color]}${hexAlpha[alpha]}`;
+
+const hexAlpha = {
+  100: 'FF',
+  99: 'FC',
+  98: 'FA',
+  97: 'F7',
+  96: 'F5',
+  95: 'F2',
+  94: 'F0',
+  93: 'ED',
+  92: 'EB',
+  91: 'E8',
+  90: 'E6',
+  89: 'E3',
+  88: 'E0',
+  87: 'DE',
+  86: 'DB',
+  85: 'D9',
+  84: 'D6',
+  83: 'D4',
+  82: 'D1',
+  81: 'CF',
+  80: 'CC',
+  79: 'C9',
+  78: 'C7',
+  77: 'C4',
+  76: 'C2',
+  75: 'BF',
+  74: 'BD',
+  73: 'BA',
+  72: 'B8',
+  71: 'B5',
+  70: 'B3',
+  69: 'B0',
+  68: 'AD',
+  67: 'AB',
+  66: 'A8',
+  65: 'A6',
+  64: 'A3',
+  63: 'A1',
+  62: '9E',
+  61: '9C',
+  60: '99',
+  59: '96',
+  58: '94',
+  57: '91',
+  56: '8F',
+  55: '8C',
+  54: '8A',
+  53: '87',
+  52: '85',
+  51: '82',
+  50: '80',
+  49: '7D',
+  48: '7A',
+  47: '78',
+  46: '75',
+  45: '73',
+  44: '70',
+  43: '6E',
+  42: '6B',
+  41: '69',
+  40: '66',
+  39: '63',
+  38: '61',
+  37: '5E',
+  36: '5C',
+  35: '59',
+  34: '57',
+  33: '54',
+  32: '52',
+  31: '4F',
+  30: '4D',
+  29: '4A',
+  28: '47',
+  27: '45',
+  26: '42',
+  25: '40',
+  24: '3D',
+  23: '3B',
+  22: '38',
+  21: '36',
+  20: '33',
+  19: '30',
+  18: '2E',
+  17: '2B',
+  16: '29',
+  15: '26',
+  14: '24',
+  13: '21',
+  12: '1F',
+  11: '1C',
+  10: '1A',
+  9: '17',
+  8: '14',
+  7: '12',
+  6: '0F',
+  5: '0D',
+  4: '0A',
+  3: '08',
+  2: '05',
+  1: '03',
+  0: '00',
+};
+
+const colors = {
+  black: '#000000',
+  white: '#FFFFFF',
+  lightest: '#F1F3FF',
+  lightest60: `#ffffff${hexAlpha[60]}`,
+  darkest: '#000534',
+};
+
+const fonts = {
+  primary: 'Helvetica, sans-serif',
+};
+
+const borderRadius = {
+  normal: toRems(3),
+  lg: toRems(12),
+  '100': '100%',
+};
+
+const zIndex = {
+  nav: 10,
+  foregroundContent: 5,
+  bleedThroughMobileNav: 20,
+  mobileNav: 15,
+};
+
+const opacity = {
+  horizontalRule: 23,
+};
+
+const gradients = {
+  sandToBlurple: 'linear-gradient(90deg, #fccb90, #d57eeb)',
+  tealToBrightPurple: 'linear-gradient(90deg, #2e9fcc, #ce69e8)',
+  sandToBrightPurpleToBlurple:
+    'linear-gradient(125.5deg, #fccb90 -26%, #d57eeb 60.6%, #7e89eb)',
+  aquaToAqua: 'linear-gradient(90deg, #6EEFF1, #6EEFF1);',
+  tealToPurple: 'linear-gradient(90deg, #2e9fcc, #ce69e8);',
+};
+
+export type Colors = keyof typeof colors;
+export type Fonts = keyof typeof fonts;
+export type BorderRadius = keyof typeof borderRadius;
+export type ZIndex = keyof typeof zIndex;
+export type Alphas = keyof typeof hexAlpha;
+export type Opacities = keyof typeof opacity;
+export type Gradients = keyof typeof gradients;
+
+const theme: DefaultTheme = {
+  color: colors,
+  font: fonts,
+  opacity,
+  zIndex,
+  borderRadius,
+  gradients,
+  toRems,
+  rhythm,
+  aColor,
+};
+
+export default theme;

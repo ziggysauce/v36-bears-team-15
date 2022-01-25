426923e92d1926fac0a9f6bc3a7bca0171037a81
Vincent Arlou <arlou@protonmail.com>
2022-01-24 21:55:27 -0500

    feat: set up supabase

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,6 +1,27 @@
 /* istanbul ignore file */
 import Main from 'components/Main';
+import supabase from '../lib/supabase';
 
-export default function Home() {
-  return <Main />;
+export default function Home({ users }) {
+  return (
+    <>
+      <Main />
+      <p>{JSON.stringify(users)}</p>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const { data: users, error } = await supabase.from('User').select('*');
+
+  if (error) {
+    console.log(error);
+  }
+
+  return {
+    props: {
+      users,
+      error,
+    },
+  };
 }
